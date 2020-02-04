66f9b1560e1e76afb0218eb8f964d8bb96a5eaae
Roger Murray <roger.murray@iis.se>
2020-02-04 16:10:48 +0100

    gets the nsids and probes that query them

--- atlas-basics/api-basic.py
+++ atlas-basics/api-basic.py
@@ -13,17 +13,42 @@ and returns a list of dns nodes responding and their response time for the query
 
 from ripe.atlas.sagan import DnsResult
 from ripe.atlas.cousteau import AtlasResultsRequest
+import dns.message
+import base64
+from collections import defaultdict
 
-kwargs = {"msm_id": 1413716, "start": 1567133400, "stop": 1567134000}
+d = defaultdict(list)
+
+
+kwargs = {"msm_id": 23912948, "start": 1580736272, "stop": 1580736603}
+# kwargs = {"msm_id": 1413716, "start": 1567133400, "stop": 1567134000}
 # kwargs = {"msm_id": 23265672, "start": 1574595600, "stop": 1574596200}
 
 is_success, results = AtlasResultsRequest(**kwargs).create()
 
 if is_success:
+    count = 0
     for result in results:
-        my_result = DnsResult(result)
-        print(
-            my_result.responses[0].abuf.answers[0].data_string
-            + " -> "
-            + str(my_result.responses[0].response_time)
-        )
+        probe = result['prb_id']
+        try:
+            answer = result['result']['abuf'] + "=="
+            content = base64.b64decode(answer)
+            msg = dns.message.from_wire(content)
+            for opt in msg.options:
+                if opt.otype == dns.edns.NSID:
+                    print(f"{probe} -> NSID: {str(opt.data)}")
+                    d[str(opt.data)].append(probe)
+
+
+
+
+        except:
+            next
+    for item in d:
+        print(f"{item} -> {d[item]}")
+        # my_result = DnsResult(result)
+        # print(
+        #     my_result.responses[0].abuf.answers[0].data_string
+        #     + " -> "
+        #     + str(my_result.responses[0].response_time)
+        # )
