704c015d9d8aba76d433a032ebdbd4d0dca8ec91
Roger Murray <roger.murray@iis.se>
2020-02-07 12:48:29 +0100

    paus

--- atlas-basics/probe_server_mapping.py
+++ atlas-basics/probe_server_mapping.py
@@ -18,10 +18,16 @@ from ripe.atlas.cousteau import AtlasResultsRequest
 import dns.message
 import base64
 from collections import defaultdict
+import os
+import sys
 
 d = defaultdict(list)
 
 def measurement_list():
+    """
+    list of measurements
+    :return:
+    """
     listan = [
         {"msm_id": 23912948, "start": 1580736272, "stop": 1580736603},
         {"msm_id": 23917314, "start": 1580891978},
@@ -32,25 +38,49 @@ def measurement_list():
 
 
 def measurement_parser(kwargs):
+    """
+    receives a ripe atlas measurement id with a start time and possibly a stop time
+    and returns a dictionary containing the nodes reached as keys and a list of probes that
+    contacted the node as values
+
+    :param kwargs:
+    :return dictionary keys = dns nodes, values = list of probes that contacted
+    the dns node:
+    """
     is_success, results = AtlasResultsRequest(**kwargs).create()
     if is_success:
-        count = 0
         for result in results:
             probe = result['prb_id']
             try:
                 answer = result['result']['abuf'] + "=="
                 content = base64.b64decode(answer)
                 msg = dns.message.from_wire(content)
+                soa = msg.answer[0] # if it is a soa query the soa will be in msg.answer[0]
                 for opt in msg.options:
                     if opt.otype == dns.edns.NSID:
                         # print(f"{probe} -> NSID: {str(opt.data)}")
-                        d[str(opt.data)].append(probe)
+                        d[str(opt.data.decode("utf-8"))].append(probe)
             except:
                  next
     return d
     # for item in d:
         # print(f"{item} -> {d[item]}")
 
+
+def check_node_inclusion(responding_nodes):
+    """
+    compare the responding nodes to the nodes listed in nodes list to confirm
+    that all node responded
+
+    :param responding_nodes:
+    :return Bool:
+    """
+    with open(os.path.join(sys.path[0], "denic_nodes"), "r") as file:
+        denic_nodes = set([node.strip() for node in file])
+        if set(responding_nodes) == denic_nodes:
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
     coll_dic = defaultdict(list)
@@ -59,7 +89,9 @@ if __name__ == '__main__':
     # for item in mapping:
         # print(f"{item} -> {mapping[item][0]}")
 
-    # listan = [mapping[x][0] for x in mapping]
-    # print(listan)
-    for key in mapping.keys():
-        print(key)
+    listan = [x for x in mapping]
+    print(listan)
+    # for key in mapping.keys():
+    #     print(key)
+
+    print(check_node_inclusion(listan))
